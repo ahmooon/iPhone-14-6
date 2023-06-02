be69d3628e285df2ef246baafcd93917b14eb224
ahmooon <132542696+ahmooon@users.noreply.github.com>
2023-06-03 02:12:31 +0900

    Nice

--- iPhone 14-6/ContentView.swift
+++ iPhone 14-6/ContentView.swift
@@ -6,83 +6,74 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        ZStack {
+            Color("AccentColor")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("Ellipse 124")
+                    Spacer()
+                    Image("Ellipse 125")
+                    Spacer()
                 }
-                .onDelete(perform: deleteItems)
+                Spacer().frame(height: 20)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer().frame(width: 81)
+                    Image("Text4")
+                    Spacer()
+                    Image("Text5")
+                    Spacer().frame(width:96)
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                Spacer().frame(height: 75)
+            }
+            HStack{
+                VStack{
+                    Image("sus")
+                    Spacer().frame(height: 675)
                 }
+                Spacer().frame(width:268)
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            HStack{
+                VStack{
+                    Image("Text")
+                    Spacer().frame(height: 625)
+                }
+                Spacer().frame(width:245)
             }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            Image("Ellipse 127")
+            Image("25")
+            VStack{
+                Image("12km")
+                Spacer().frame(height: 210)
+            }
+            VStack{
+                Image("Vector 259")
+                Spacer().frame(height:575)
+            }
+            VStack{
+                HStack{
+                    Spacer().frame(width: 20)
+                    Image("Text2")
+                    Spacer()
+                    Image("Text3")
+                    Spacer().frame(width: 20)
+                }
+                Spacer().frame(height:525)
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
